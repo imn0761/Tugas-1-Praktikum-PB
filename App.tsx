881299ba0ac6b752f4094feed7e89aff34c5db72
narindraaulia <imn0761@gmail.com>
2024-05-09 20:14:21 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { Alert, Button, Image, Text, TextInput, View } from "react-native";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const ValidasiPalindrom = () => {
+  const [inputText, setInputText] = useState('');
+  const [isValidPalindrome, setIsValidPalindrome] = useState(false);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleInputChange = (text) => {
+    setInputText(text);
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+  const validatePalindrome = () => {
+    const reversedText = inputText.split('').reverse().join('');
+    setIsValidPalindrome(inputText === reversedText);
+  };
+
+return(
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop : -250,
+    }}>
+
+    <Text style={{
+      fontSize : 25,
+      fontWeight : 'bold',
+      color : 'blue',
+    }}>Hallo</Text>
+
+    <Image style = {{
+      width : 200,
+      height : 200,
+    }}
+      source = {{uri: 'https://upload.wikimedia.org/wikipedia/en/0/0d/Simpsons_FamilyPicture.png'
+    }}/>
+
+      <TextInput style = {{
+        borderWidth : 1,
+        borderColor : 'Black',
+        width : 200,
+        height : 40,
+        padding : 10,
+        marginTop : 30,
+        marginBottom : 10,
+        borderRadius : 10,
+      }}
+        placeholder="Masukkan Kata"
+        value={inputText}
+        onChangeText={handleInputChange}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+
+      <Button title="Validate" onPress={validatePalindrome} />
+      {isValidPalindrome? (
+        <Text style={{
+          color : 'green',
+          margin : -110,
+        }}>Terdeteksi Palindrome!</Text>
+      ) : (
+        <Text style={{
+          color : 'red',
+          margin : -110,
+        }}>TIDAK Terdeteksi Palindrome!</Text>
+      )}
+      
+    </View>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+};
+export default ValidasiPalindrom;
